2f04a5ba11bd5549f72632f04e0dc4823288d024
Anand <anandbaskaran3193@gmail.com>
2023-05-12 16:21:36 +0200

    Revert "Add firebase config"

--- src/@db/db.js
+++ src/@db/db.js
@@ -1,10 +1,10 @@
-// import './data/assets/currentAssets.ts';
-// import './data/assets/fixedDeposits.ts';
-// import './data/assets/stocks.ts';
-// import './data/assets/mutualFunds.ts';
-// import './data/assets/pms.ts';
-// import './data/assets/assets.ts';
+import './data/assets/currentAssets.ts';
+import './data/assets/fixedDeposits.ts';
+import './data/assets/stocks.ts';
+import './data/assets/mutualFunds.ts';
+import './data/assets/pms.ts';
+import './data/assets/assets.ts';
 import mock from './mock';
-// /* eslint-enable import/extensions */
-//
+/* eslint-enable import/extensions */
+
 mock.onAny().passThrough(); // forwards the matched request over network
